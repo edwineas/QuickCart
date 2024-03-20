dac2055e4bfa9190eaca0f104d69edd12bc1b76d
edwineas <edwinalexshaji@gmail.com>
2024-03-20 22:26:40 +0530

    cart final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,18 +3,21 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './Cart.css';
 
 const Cart = () => {
-  return (
-    <div className='cart-div'>
-        <ShoppingCartIcon 
-            className='cart-icon'
-            sx={{ fontSize: 40, color: '#435BDA' }
-        }
-        />
-        <div className="cart-count-div">
-            <p className='cart-count'>1</p>
+    return (
+        <div className="bottomCart">
+            <div className='cart-div'>
+                <ShoppingCartIcon
+                    className='cart-icon'
+                    sx={{ fontSize: 40, color: '#435BDA' }
+                    }
+                />
+                <div className="cart-count-div">
+                    <p className='cart-count'>1</p>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+
+    )
 }
 
 export default Cart
