11adaf5bdf03ffce77fab9b28893664b343ba935
edwineas <edwinalexshaji@gmail.com>
2024-03-20 22:16:08 +0530

    cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Height } from '@mui/icons-material';
+import './Cart.css';
 
 const Cart = () => {
   return (
     <div className='cart-div'>
-        <ShoppingCartIcon className='cart-icon'/>
-        <p className='cart-count'>1</p>
+        <ShoppingCartIcon 
+            className='cart-icon'
+            sx={{ fontSize: 40, color: '#435BDA' }
+        }
+        />
+        <div className="cart-count-div">
+            <p className='cart-count'>1</p>
+        </div>
     </div>
   )
 }
