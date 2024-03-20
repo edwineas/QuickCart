b83bf0ccf708a81749d4aafbcdec1788f68baaf9
Gokul-Krishna-B-7 <gokulkrishnabiju@gmail.com>
2024-03-20 23:10:43 +0530

    updated btn

--- src/pages/Third/Checkout.jsx
+++ src/pages/Third/Checkout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Priority from '../../components/PrioritySelector/Priority';
 import Navbar from "../../components/Navbar/Navbar";
+import Items from "../../components/CheckOutItem/CheckoutItem"
 const Checkout = () => {
     return (
     <>    
     <Navbar />
     <Priority />
+    <Items />
     </>
     );
 };
