d34eca36a247a2d836bbc4f022610965bc5cea48
edwineas <edwinalexshaji@gmail.com>
2024-03-25 19:11:31 +0530

    First name changed to Feed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ShopListBody from './pages/Second/ShopListBody';
 import './App.css';
-import First from './pages/First/First';
+import First from './pages/Feed/Feed';
 import Checkout from './pages/Third/Checkout';
 import Cart from './components/Cart/Cart';
 import Login from './pages/Login/Login';
 import RegisterShop from './pages/Register/RegisterShop';
 import CartBill from './pages/Confirmation/CartBill';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <First />,
-  },
-  {
-    path: '/shop-list',
-    element: <ShopListBody />,
-  },
-  {
-    path: '/checkout',
-    element: <Checkout />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/registershop',
-    element: <RegisterShop />,
-  },
-  {
-    path: '/cartbill',
-    element: <CartBill />,
-  },
-]);
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-
-
+  
   return (
     <>
-      <RouterProvider router={router} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<First />} />
+          <Route path="/shop-list" element={<ShopListBody />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registershop" element={<RegisterShop />} />
+          <Route path="/cartbill" element={<CartBill />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/Feed/Feed.jsx
@@ -0,0 +1,22 @@
+
+import Body from '../../components/FirstBody/Body.jsx';
+import Navbar from '../../components/Navbar/Navbar.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
+
+function Feed() {
+
+
+  return (
+    <>
+      <div className="app-container">
+        <Body />
+
+      </div>
+      <Footer />
+    </>
+  )
+
+}
+
+
+export default Feed;
