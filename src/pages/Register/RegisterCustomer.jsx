99208a2c93706faa2cbfe6f025eafa98cd52ef33
JERISHAB <jerishabmayan@gmail.com>
2024-03-26 11:45:43 +0700

    customer signup completed

--- src/pages/Register/RegisterCustomer.jsx
+++ src/pages/Register/RegisterCustomer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './RegisterCustomer.css';
 
 const RegisterCustomer = () => {
   return (
-    <div>RegisterCustomer</div>
-  )
-}
+    <div className="registerCustomer-container">
+        <h1 className='registerCustomer-heading'>Customer Signup</h1>
+      <form className="registerCustomer-form">
+        <input className="registerCustomer-input" type="text" placeholder="Phone Number" />
+        <input className="registerCustomer-input" type="text" placeholder="Username" />
+        <input className="registerCustomer-input" type="password" placeholder="Password" />
+        <input className="registerCustomer-input" type="password" placeholder="confirm password" />
+        <button className="registerCustomer-button" type="submit">Confirm</button>
+      </form>
+    </div>
+  );
+};
 
-export default RegisterCustomer
+export default RegisterCustomer;
