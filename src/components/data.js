41bec63cb8b2d1c438adb4e71f8eba2ca1c83cc6
edwineas <edwinalexshaji@gmail.com>
2024-04-15 23:07:55 +0530

    shop listing complete

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 
+//List of products
 export const useProducts = () => {
   const [products, setProducts] = useState([]);
 
@@ -26,4 +27,29 @@ export const useProducts = () => {
   }, []);
 
   return products;
+};
+
+//List of shops
+export const useShops = () => {
+  const [shops, setShops] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/shops/list/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (Array.isArray(data)) {
+          setShops(data);
+        } else {
+          console.error('Data is not an array:', data);
+        }
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return shops;
 };
--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -2,28 +2,31 @@ import React, { useState, useEffect } from "react";
 import { Cart, ShopsGrid, Popup } from "../../components";
 import "./Feed.css";
 import { Carrot, Cauliflower, Onion, sampleImage as Img } from "../../images";
-import { useProducts } from "../../components/data";
+import { useProducts,useShops } from "../../components/data";
 
 
-const shops = [
-  { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
-  { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
-  { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
-  { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
-  { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
-  { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
-  { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
-  { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
-  { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
-  { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
-  { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
-  { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
-];
+// const shops = [
+//   { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
+//   { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
+//   { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
+//   { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
+//   { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
+//   { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
+//   { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
+//   { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
+//   { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
+//   { src: Img, name: "Elite Supermarket 1", rating: 4.5 },
+//   { src: Img, name: "Elite Supermarket 2", rating: 3.8 },
+//   { src: Img, name: "Elite Supermarket 3", rating: 4.2 },
+// ];
+
 
 function Feed() {
   const [selectedProduct, setSelectedProduct] = useState(null);
 
   const products = useProducts();
+  const shops = useShops();
+  // console.log(shops)
   
   const itemClick = (product) => {
     setSelectedProduct(product); // Update selected image on click
@@ -58,7 +61,7 @@ function Feed() {
             {shops.map((shop, index) => (
               <ShopsGrid
                 key={index}
-                shopSrc={shop.src}
+                shopSrc={shop.image}
                 shopName={shop.name}
                 shopRating={shop.rating}
               />
