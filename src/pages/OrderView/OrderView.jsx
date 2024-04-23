c5a326b4083154e956c288d05e4594002f448333
edwineas <edwinalexshaji@gmail.com>
2024-04-23 15:04:38 +0530

    shop order view

--- src/pages/OrderView/OrderView.jsx
+++ src/pages/OrderView/OrderView.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './OrderView.css'; // Assuming you have a CSS file named OrderView.css in the same directory
 import { Table } from '../../components';
 
 const OrderView = () => {
-    const dummyData = [
-        { date: '2024-04-16', orderId: '123', username: 'dinesh', totalCost: '$100.00', orderDetails: 'Sample Details' },
-        // Add more dummy data here
-    ];
+    const [shoporders, setShopOrders] = useState([]);
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/orders/shop/13/');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                const dataWithStatus = data.map(item => {
+                    const date = new Date(item.date);
+                    const formattedDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+                    return {
+                        order: item.order,
+                        shop_price: item.shop_price,
+                        status: !item.packet_picked ? 'Waiting for Pickup' : !item.payment_received ? 'Waiting for Payment' : 'Complete',
+                        date: formattedDate,
+                        customer_name: item.customer_name,
+                    };
+                });
+                setShopOrders(dataWithStatus);
+            } catch (error) {
+                console.error('Failed to fetch orders:', error);
+            }
+        };
+
+        fetchOrders();
+    }, []);
 
     return (
         <>
             <div className="order-view">
                 <h1>OrderView</h1>
                 <Table
-                    titles={['Date', 'Order ID', 'User NAME', 'Total Cost']}
-                    keys={['date', 'orderId', 'username', 'totalCost']}
-                    data={dummyData}
+                    titles={['Order ID', 'Date', 'Customer Name', 'Price', 'Status']}
+                    keys={['order','date', 'customer_name', 'shop_price', 'status']}
+                    data={shoporders}
                     showAction
                     actionTitle="Order Details"
                     buttonLabel="View Details"
-                    onButtonClick={(row) => alert(row.orderDetails)}
+                    onButtonClick={(row) => alert(JSON.stringify(row))}
                 />
             </div>
         </>
