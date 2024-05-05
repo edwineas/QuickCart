b7a698bbe975bbe1f0cb9c09560cbe9dc2a285ad
edwineas <edwinalexshaji@gmail.com>
2024-05-06 00:44:08 +0530

    completed customer order

--- src/pages/PostOrder/PostOrder.jsx
+++ src/pages/PostOrder/PostOrder.jsx
@@ -9,11 +9,7 @@ const PostOrder = () => {
   const location = useLocation();
   const { orderId } = location.state;
   const [orders, setOrders] = useState([]);
-  const handleClick = () => {
-    navigate('/thankyou')
-  }
-
-
+  const [reload, setReload] = useState(false);
 
   useEffect(() => {
     const postOrder = async () => {
@@ -27,7 +23,29 @@ const PostOrder = () => {
     };
     console.log('order', orders)
     postOrder();
-  }, [orderId]);
+  }, [orderId, reload]);
+
+  const handlePacket = async (ordershopid) => {
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_DJANGO_URL}/orders/packet/${ordershopid}/`);
+      console.log('Packet picked:', response.data);
+      setReload(!reload); // Toggle the reload state
+    } catch (error) {
+      console.error('Error picking packet:', error);
+    }
+  }
+
+  const handlePayment = async (ordershopid) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_DJANGO_URL}/orders/checkpayment/${ordershopid}/`);
+      console.log('Payment checked:', response.data);
+      if (response.data.payment_received === true) {
+        setReload(!reload); // Toggle the reload state
+      }
+    } catch (error) {
+      console.error('Error checking payment:', error);
+    }
+  }
 
   return (
     <div className="postorder">
@@ -38,14 +56,36 @@ const PostOrder = () => {
           {shop.products.map((item, index) => (
             <div key={index} className="item">
               <span>{item.product_name}</span>
-              <span>{item.product_quantity}</span>
-              <span>{item.product_price}</span>
+              <div className='itemqp'>
+                <span>{item.product_quantity}</span>
+                <span>{item.product_price}</span>
+              </div>
             </div>
           ))}
           <div className='totaldiv'><p className="total">Total: {shop.shop_price}</p></div>
-          <button className="status" onClick={handleClick}>{shop.packet_picked ? 'Picked' : 'Not Picked'}</button>
+          <div className='statusdiv'>
+            <button
+              className={`status ${shop.packet_picked ? 'completed' : ''}`}
+              onClick={() => handlePacket(shop.order_shop_id)}
+              disabled={shop.packet_picked}
+            >Packet Picked</button>
+            <button
+              className={`status ${shop.payment_received ? 'completed' : ''}`}
+              onClick={() => handlePayment(shop.order_shop_id)}
+              disabled={shop.payment_received}
+            >Payment Done</button>
+          </div>
         </div>
       ))}
+      {orders.every(order => order.packet_picked && order.payment_received) && (
+        <button
+          className="continue"
+          style={{ width: '100%', fontWeight: '600' }}
+          onClick={() => navigate('/thankyou')} // Add this line
+        >
+          Continue
+        </button>
+      )}
     </div>
   );
 }
